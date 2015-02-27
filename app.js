200b5db03a563b0f9f11a4e9f47f330488a4bcef
Kevin Brightwell <kevin.brightwell2@gmail.com>
2015-02-27 18:31:24 -0500

    Fix app.js

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var querystring = require('querystring');
 var app = express();
 app.use(bodyParser.json());
 
+app.set('port', (process.env.PORT || 8080));
+app.use(express.static(__dirname + '/public'));
+
 /* ***********************************
  * Static routes
  * ***********************************/
@@ -117,4 +120,6 @@ app.get('/status/:id', function (req, res) {
     //delete sessions[req.params.id];
 });
 
-app.listen(8080);
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'));
+});
