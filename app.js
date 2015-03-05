fcfca12ebf0d361ddfb5a890c0eda31c3ed48929
Kevin Brightwell <kevin.brightwell2@gmail.com>
2015-03-05 10:39:55 -0500

    Make application work as node app..
    * Moved files to /public
    * Add parsing of HTML crap into JSON format
    * Add more tests

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
     _ = require('underscore'),
-    request = require('superagent');
+    request = require('superagent'),
+    cheerio = require('cheerio');
 
 var http = require('http');
 var querystring = require('querystring');
@@ -44,12 +45,60 @@ var baseQuery = {
     s : 'generic'
 };
 
-var test = {
-    table: ['ah', 'td', 'jh'],
-    hands: [['ac', 'jd'], ['as', 'qs']]
-};
-
 var sessions = {};
+
+
+var parseHTMLResult = (function () {
+
+    var R_TRIALS = /((?:\d{1,3},)*\d{1,3})\s+trials\s+\((\w+)\)/;
+    var R_CARD_STR = '(?:[1-9tjqka][scdh]|(?:100|\\d?\\d)%)';
+    var R_BOARD = new RegExp('board:\\s+((?:' + R_CARD_STR + ')*)');
+    var R_CARD_CAP = new RegExp('(' + R_CARD_STR + ')');
+
+    function splitHand(input) {
+        return _.compact(input.split(R_CARD_CAP));
+    }
+
+    return function __parseHTMLResult(html) {
+        var $ = cheerio.load(html);
+
+        var o = {};
+        var trialsResult = R_TRIALS.exec($('.pptSimTrials').text());
+        o.trials = Number(trialsResult[1].replace(/,/g, ''));
+        o.exhaustive = trialsResult[2] === 'Exhaustive';
+
+        var boardStrSpl = R_BOARD.exec($('.pptBoard').text());
+        if (boardStrSpl == null) {
+            o.board = { };
+        } else {
+            var tableCards = splitHand(boardStrSpl[1]);
+            o.board = {
+                flop: _.first(tableCards, 3),
+                turn: tableCards[3],
+                river: tableCards[4]
+            };
+        }
+
+        var A = 'A'.charCodeAt(0);
+        o.hands = _.chain(_.range(A, A + 6))
+            .map(function (c) { return '.simRow' + String.fromCharCode(c); })
+            .map(function (selector) {
+
+                if ($(selector).length > 0) {
+                    return {
+                        hand: splitHand($(selector + ' .pptSpec').text()),
+                        equity: Number($(selector + ' .pptEV').text().slice(0, -1)),
+                        wins: Number($(selector + ' .pptWinsHi').text().replace(/,/g, '')),
+                        ties: Number($(selector + ' .pptTiesHi').text().replace(/,/g, ''))
+                    };
+                } else {
+                    return undefined;
+                }
+            }).compact().value();
+
+        return o;
+    };
+})();
 
 /**
  * Expected input: {
@@ -59,10 +108,22 @@ var sessions = {};
  */
 app.post('/submit', function (req, res) {
 
-    req.accepts('application/json');
+    if (!req.is('json')) {
+        res.sendStatus(406);
+        return;
+    }
 
     var data = req.body;
-    if (_.isUndefined(data.hands)) data = test;
+
+    if (!_.isObject(data) ||
+        (!_.isArray(data.hands))) {
+        res.sendStatus(400);
+
+        return;
+    }
+
+    // default the table
+    data = _.defaults(data, { table: {} });
 
     var hands = _.chain(_.range(1, 7)).map(function(i) {
         if (i <= data.hands.length) {
@@ -72,21 +133,24 @@ app.post('/submit', function (req, res) {
         return ['h' + i, ''];
     }).object().value();
 
+    var postObj = _.extend(baseQuery, hands, {
+        b : _.flatten(['', data.table.flop, data.table.turn, data.table.river], true).join('')
+    });
     // create the post data (key value) pairs
-    var postData = querystring.stringify(_.extend(baseQuery, hands, { b : data.table.join('') }));
+    var postQuery = querystring.stringify(postObj);
 
     var sessionId = _.uniqueId('session_');
     sessions[sessionId] = "Running";
 
     request.post('http://www.propokertools.com/simulations/results_box')
-        .set('content-length', postData.length)
+        .set('content-length', postQuery.length)
         .set('X-Requested-With', 'XMLHttpRequest') // required
-        .send(postData)
+        .send(postQuery)
         .end(function (res) {
             if (res.error)
                 throw res.error;
 
-            sessions[sessionId] = res.text; // html
+            sessions[sessionId] = parseHTMLResult(res.text); // html
 
             //console.log('STATUS: ' + _res.statusCode);
             //console.log('HEADERS: ' + JSON.stringify(_res.headers));
--- test/api.js
+++ test/api.js
@@ -10,7 +10,9 @@ describe('/submit POST', function () {
         request(url)
             .post('/submit')
             .send({
-                table: ['ah', 'td', 'jh'],
+                table: {
+                   flop: ['ah', 'td', 'jh']
+                },
                 hands: [['ac', 'jd'], ['as', 'qs']]
             })
             .expect('Content-Type', /json/)
@@ -20,10 +22,54 @@ describe('/submit POST', function () {
                     throw err;
                 }
 
-                should(res.body).have.property('id').and.be.a.String;
+                res.body.should.have.property('id').and.be.a.String;
 
                 done();
             });
+    });
+
+    it('with no table should return an ID in JSON format', function (done) {
+        request(url)
+            .post('/submit')
+            .send({
+                hands: [['ac', 'jd'], ['as', 'qs']]
+            })
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    throw err;
+                }
+
+                res.body.should.have.property('id').and.be.a.String;
+
+                done();
+            });
+    });
+
+    it('with no hands should fail with 400', function (done) {
+        request(url)
+            .post('/submit')
+            .send({ })
+            .expect(400)
+            .end(done);
+    });
+
+    it('with no data should fail with 400', function (done) {
+        request(url)
+            .post('/submit')
+            .set('content-type', 'application/json')
+            .send()
+            .expect(400)
+            .end(done);
+    });
+
+    it('with invalid data format should fail with 406', function (done) {
+        request(url)
+            .post('/submit')
+            .send()
+            .expect(406)
+            .end(done);
     });
 });
 
@@ -37,8 +83,8 @@ describe('/status GET', function () {
             .end(function (err, res) {
                 if (err) throw err;
 
-                should(res.body).have.property('id').and.be.a.String;
-                should(res.body).have.property('message').and.be.a.String;
+                res.body.should.have.property('id').and.be.a.String;
+                res.body.should.have.property('message').and.be.a.String;
 
                 assert(/^Error/.test(res.body.message));
                 done();
@@ -49,8 +95,12 @@ describe('/status GET', function () {
         request(url)
             .post('/submit')
             .send({
-                table: ['ah', 'td', 'jh'],
-                hands: [['ac', 'jd'], ['as', 'qs']]
+                table: {
+                    flop: ['ah', 'td', 'jh'],
+                    turn: 'jd',
+                    river: 'qd'
+                },
+                hands: [['ac', 'as'], ['90%']]
             })
             .expect('content-type', /json/)
             .expect(200)
@@ -59,7 +109,7 @@ describe('/status GET', function () {
                     throw err;
                 }
 
-                should(res.body).have.property('id').and.be.a.String;
+                res.body.should.have.property('id').and.be.a.String;
                 var id = res.body.id;
 
                 var failNow = false;
@@ -76,15 +126,54 @@ describe('/status GET', function () {
                                 throw err;
                             }
 
-                            should(res.body).have.property('id').and.be.a.String;
-                            should(res.body).have.property('message').and.be.a.String;
+                            var body = res.body;
+
+                            body.should.have.property('id').and.match(/session_/);
+                            body.should.have.property('message');
 
                             assert(!failNow);
 
                             if (/^Running/.test(res.body.message)) {
                                 setTimeout(callReq, 100);
+
+
                             } else {
-                                assert(/^\s*<div class='onesimbox'/.test(res.body.message));
+                                var msg = body.message;
+                                msg.should.have.property('board');
+                                msg.should.have.property('exhaustive').equal(true);
+                                msg.should.have.property('hands').with.lengthOf(2);
+                                msg.should.have.property('trials').and.be.equal(849);
+
+                                var board = msg.board;
+                                board.should.have.property('flop').containDeep(['ah', 'td', 'jh']);
+                                board.should.have.property('turn').be.equal('jd');
+                                board.should.have.property('river').be.equal('qd');
+
+                                msg.hands.forEach(function (h) {
+                                    h.should.have.property('hand').be.an.Array;
+                                    h.hand.length.should.be.within(1, 2);
+                                    h.should.have.property('equity').be.within(0, 100.0);
+                                    h.should.have.property('wins').be.Number;
+                                    h.should.have.property('ties').be.Number;
+                                });
+
+                                msg.hands.should.containEql({
+                                        hand: [
+                                            "ac",
+                                            "as"
+                                        ],
+                                        equity: 99.53,
+                                        wins: 845,
+                                        ties: 0
+                                    }).and.containEql({
+                                        hand: [
+                                            "90%"
+                                        ],
+                                        equity: 0.47,
+                                        wins: 4,
+                                        ties: 0
+                                    });
+
                                 done();
                             }
                         });
